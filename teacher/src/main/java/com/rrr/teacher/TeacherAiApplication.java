12f404fd79cb317a8b9388e11852e1e21e55ff68
Robert-Sobolewski <84735293+Robert-Sobolewski@users.noreply.github.com>
2025-01-15 14:55:11 +0100

    init backend

---
+++ teacher/src/main/java/com/rrr/teacher/TeacherAiApplication.java
@@ -0,0 +1,13 @@
+package com.rrr.teacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherAiApplication.class, args);
+	}
+
+}
---
+++ teacher/src/test/java/com/rrr/teacher/TeacherAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrr.teacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
